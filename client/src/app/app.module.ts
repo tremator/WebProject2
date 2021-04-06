88240871d4465e4e250c3a8987d37306aeb58f85
Ester <evargasrue@gmail.com>
2021-04-06 16:46:33 -0600

    routes updated

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,28 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { BarChartComponent } from './components/bar-chart/bar-chart.component';
-import { LineChartComponent } from './components/line-chart/line-chart.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { NotificationDropdownComponent } from './components/notification-dropdown/notification-dropdown.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { UserDropdownComponent } from './components/user-dropdown/user-dropdown.component';
 import { ProjectsBoardComponent } from './projects-board/projects-board.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsComponent,
-    DashboardComponent,
-    BarChartComponent,
-    LineChartComponent,
-    NavbarComponent,
-    NotificationDropdownComponent,
-    SidebarComponent,
-    UserDropdownComponent,
-    ProjectsBoardComponent
+    ProjectsBoardComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
